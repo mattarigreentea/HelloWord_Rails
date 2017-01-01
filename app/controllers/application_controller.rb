f729bc38b865e8be2630b1073ee6744e7d73fb28
mattarigreentea <mattarigreentea0106@gmail.com>
2017-01-02 05:18:18 +0900

    文言を表示できるように追加
    トップページの文言を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  def hello
+    name = "torinist"
+    render text: "Hello, #{name}, Welcome to Ruby!"
+  end
 end
